db55e72db7ba82da884688234d668506780a0b43
danny-flahive <danny.flahive@iris.co.uk>
2021-11-07 19:42:52 +0000

    Ex 3.3 Allow user to input two values to multiply

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Calculator!");
+            Console.Write("Enter the first number to multiply: ");
+            int first_multiplier = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number to multiply: ");
+            int second_multiplier = int.Parse(Console.ReadLine());
+            int result = first_multiplier * second_multiplier;
+            Console.WriteLine(result);
+        }
+    }
+}
