b23005516b8b397fc3f6a26dbe73c3f7f3758b08
danny-flahive <danny.flahive@iris.co.uk>
2021-11-07 19:44:30 +0000

    Ex 4.2 Allow user to specify an operator

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -7,12 +7,70 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator!");
-            Console.Write("Enter the first number to multiply: ");
-            int first_multiplier = int.Parse(Console.ReadLine());
-            Console.Write("Enter the second number to multiply: ");
-            int second_multiplier = int.Parse(Console.ReadLine());
-            int result = first_multiplier * second_multiplier;
-            Console.WriteLine(result);
+            //Input sanitisation on all three user inputs before the operation is performed
+            string chosenOperator = "";
+            while (chosenOperator != "+" && chosenOperator != "-" && chosenOperator != "*" && chosenOperator != "/")
+            {
+                Console.Write("Enter an operator (+, -, *, /): ");
+                chosenOperator = Console.ReadLine();
+            }
+
+            Boolean validInput = false;
+            int firstValue = 0;
+            while (!validInput)
+            {
+                Console.Write("Enter the first number: ");
+                try
+                {
+                    firstValue = int.Parse(Console.ReadLine());
+                    validInput = true;
+                }
+                catch (FormatException e)
+                {
+                    continue;
+                }
+            }
+
+            validInput = false;
+            int secondValue = 0;
+            while (!validInput)
+            {
+                Console.Write("Enter the second number: ");
+                try
+                {
+                    secondValue = int.Parse(Console.ReadLine());
+                    if (chosenOperator == "/" && secondValue == 0)
+                    {
+                        Console.WriteLine("Cannot divide by 0.");
+                        continue;
+                    }
+                    validInput = true;
+                }
+                catch (FormatException e)
+                {
+                    continue;
+                }
+
+            }
+
+            int output = 0;
+            switch (chosenOperator)
+            {
+                case ("+"):
+                    output = firstValue + secondValue;
+                    break;
+                case ("-"):
+                    output = firstValue - secondValue;
+                    break;
+                case ("*"):
+                    output = firstValue * secondValue;
+                    break;
+                case ("/"):
+                    output = firstValue / secondValue;
+                    break;
+            }
+
+            Console.WriteLine(output);
         }
     }
 }
