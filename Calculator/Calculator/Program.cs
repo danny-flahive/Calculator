05ff37179cee31f6083f268b3f94298089364a10
danny-flahive <danny.flahive@iris.co.uk>
2021-11-07 19:50:51 +0000

    Ex 5 Take multiple inputs to be used with 1 operator

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -7,7 +7,6 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator!");
-            //Input sanitisation on all three user inputs before the operation is performed
             string chosenOperator = "";
             while (chosenOperator != "+" && chosenOperator != "-" && chosenOperator != "*" && chosenOperator != "/")
             {
@@ -15,62 +14,73 @@ namespace Calculator
                 chosenOperator = Console.ReadLine();
             }
 
-            Boolean validInput = false;
-            int firstValue = 0;
-            while (!validInput)
-            {
-                Console.Write("Enter the first number: ");
-                try
-                {
-                    firstValue = int.Parse(Console.ReadLine());
-                    validInput = true;
-                }
-                catch (FormatException e)
-                {
-                    continue;
-                }
-            }
+            //Need to sanitise this to keep it in line with the rest of the program
+            Console.Write("How many integers would you like to enter? ");
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            int[] values = new int[numberOfInputs];
 
-            validInput = false;
-            int secondValue = 0;
-            while (!validInput)
+            //Gather all values for the calculation later
+            for (int i = 0; i < numberOfInputs; i++)
             {
-                Console.Write("Enter the second number: ");
-                try
+                Boolean validInput = false;
+                int value = 0;
+                //Input sanitisation
+                while (!validInput)
                 {
-                    secondValue = int.Parse(Console.ReadLine());
-                    if (chosenOperator == "/" && secondValue == 0)
+                    Console.Write("Enter number {0}: ", i + 1);
+                    try
                     {
-                        Console.WriteLine("Cannot divide by 0.");
+                        value = int.Parse(Console.ReadLine());
+                        if (i > 0 && chosenOperator == "/" && value == 0)
+                        {
+                            Console.WriteLine("Cannot divide by 0.");
+                            continue;
+                        }
+                        validInput = true;
+                    }
+                    catch (FormatException e)
+                    {
                         continue;
                     }
-                    validInput = true;
                 }
-                catch (FormatException e)
-                {
-                    continue;
-                }
-
+                values[i] = value;
             }
 
+            //Perform calculations
             int output = 0;
             switch (chosenOperator)
             {
                 case ("+"):
-                    output = firstValue + secondValue;
+                    foreach (int value in values)
+                    {
+                        output += value;
+                    }
                     break;
                 case ("-"):
-                    output = firstValue - secondValue;
+                    output = values[0];
+                    for (int i = 1; i < values.Length; i++)
+                    {
+                        output -= values[i];
+                    }
                     break;
                 case ("*"):
-                    output = firstValue * secondValue;
+                    output = 1;
+                    foreach (int value in values)
+                    {
+                        output *= value;
+                    }
                     break;
                 case ("/"):
-                    output = firstValue / secondValue;
+                    output = values[0];
+                    for (int i = 1; i < values.Length; i++)
+                    {
+                        output /= values[i];
+                    }
                     break;
             }
 
             Console.WriteLine(output);
+
         }
     }
 }
