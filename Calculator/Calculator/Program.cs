557e7d096f54969a3f75f6b13004d9c7ba739300
danny-flahive <danny.flahive@iris.co.uk>
2021-11-08 12:36:43 +0000

    Ex 6. Refactored code and added the ability to perform multiple calculations

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,47 +6,27 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            PrintWelcomeMessage();
+            bool performCalculations = true;
+            while (performCalculations)
+            {
+                PerformOneCalculation();
+                performCalculations = AskToContinue();
+                Console.Clear();
+            }
+
+        }
+
+        public static void PrintWelcomeMessage()
+        {
             Console.WriteLine("Welcome to the Calculator!");
-            string chosenOperator = "";
-            while (chosenOperator != "+" && chosenOperator != "-" && chosenOperator != "*" && chosenOperator != "/")
-            {
-                Console.Write("Enter an operator (+, -, *, /): ");
-                chosenOperator = Console.ReadLine();
-            }
+            Console.WriteLine("==========================");
+        }
 
-            //Need to sanitise this to keep it in line with the rest of the program
-            Console.Write("How many integers would you like to enter? ");
-            int numberOfInputs = int.Parse(Console.ReadLine());
-            int[] values = new int[numberOfInputs];
-
-            //Gather all values for the calculation later
-            for (int i = 0; i < numberOfInputs; i++)
-            {
-                Boolean validInput = false;
-                int value = 0;
-                //Input sanitisation
-                while (!validInput)
-                {
-                    Console.Write("Enter number {0}: ", i + 1);
-                    try
-                    {
-                        value = int.Parse(Console.ReadLine());
-                        if (i > 0 && chosenOperator == "/" && value == 0)
-                        {
-                            Console.WriteLine("Cannot divide by 0.");
-                            continue;
-                        }
-                        validInput = true;
-                    }
-                    catch (FormatException e)
-                    {
-                        continue;
-                    }
-                }
-                values[i] = value;
-            }
-
-            //Perform calculations
+        public static void PerformOneCalculation()
+        {
+            string chosenOperator = GetOperator();
+            int[] values = GetValues(chosenOperator);
             int output = 0;
             switch (chosenOperator)
             {
@@ -79,8 +59,83 @@ namespace Calculator
                     break;
             }
 
-            Console.WriteLine(output);
+            Console.WriteLine("The result is {0}", output);
+        }
 
+        public static string GetOperator()
+        {
+            string chosenOperator = "";
+            while (chosenOperator != "+" && chosenOperator != "-" && chosenOperator != "*" && chosenOperator != "/")
+            {
+                Console.Write("Enter an operator (+, -, *, /): ");
+                chosenOperator = Console.ReadLine();
+            }
+            return chosenOperator;
+        }
+
+        public static int[] GetValues(string userOperator)
+        {
+            int numberOfInputs;
+            do
+            {
+                numberOfInputs = GetIntInput("How many integers would you like to enter? (2 or more) ");
+            } while (numberOfInputs < 2);
+            int[] values = new int[numberOfInputs];
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+
+                int value;
+                //Prevent divide by zero errors
+                if (userOperator == "/" && i > 0)
+                {
+                    value = GetValidDivisor(i);
+                }
+                else
+                {
+                    value = GetIntInput(String.Format("Enter number {0}: ", i + 1));
+                }
+                values[i] = value;
+            }
+            return values;
+        }
+
+        public static bool AskToContinue()
+        {
+            Console.WriteLine("Would you like to perform another calculation? (Y/N)");
+            String answer = Console.ReadLine();
+            while (answer != "Y" && answer != "N")
+            {
+                Console.WriteLine("Enter a valid response (Y/N)");
+                answer = Console.ReadLine();
+            }
+            return answer == "Y";
+        }
+
+        public static int GetIntInput(string message)
+        {
+            int output = 0;
+            bool valid = false;
+            while (!valid)
+            {
+                Console.WriteLine(message);
+                valid = int.TryParse(Console.ReadLine(), out output);
+            }
+            return output;
+        }
+
+        public static int GetValidDivisor(int currentNum)
+        {
+            int value = 0;
+            bool divByZero = true;
+            while (divByZero)
+            {
+                value = GetIntInput(String.Format("Enter number {0}: ", currentNum + 1));
+                if (value != 0)
+                {
+                    divByZero = false;
+                }
+            }
+            return value;
         }
     }
 }
